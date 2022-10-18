776cfbaffb8accbfee7e200b280c5b40fc341c06
Mengxiaozhi <mmengxiaozhi@icloud.com>
2022-10-18 16:19:18 +0800

    htmlhtml

--- login.js
+++ login.js
@@ -5,15 +5,15 @@ var hb1 = document.getElementById("helpBlock1");
 var hb2 = document.getElementById("helpBlock2");
 var hb3 = document.getElementById("helpBlock3");
 var xmlHttp = new XMLHttpRequest();
-var jsonObj = null;
+var jsonObj = null; //將物件設定為空
 xmlHttp.onload = function(){
     if(xmlHttp.readyState == 4){ //表示响应完成
         if(xmlHttp.status == 200){ //响应码为200  表示服务器正确响应
             //获取响应内容
-            txt = xmlHttp.responseText ;
+            var txt = xmlHttp.responseText ;
             //把json字符串解析成json对象
             jsonObj = JSON.parse(txt);
-            console.info(jsonObj)
+            console.info(jsonObj)//把json字符串显示在console中
         }else{
             console.info('数据返回失败！状态代码：'+xmlHttp.status+'状态信息：'+xmlHttp.statusText);
         }
@@ -34,13 +34,13 @@ loginBtn.onclick = function(){
     else if(!pwd){
         hb2.style.display = 'block';
     }
-    //對比json數據，登录成功
+    //對比console中的json數據，登录成功
     else{
         for(var i=0;i<jsonObj.length;i++){
            if( nhi_id==jsonObj[i]['name'] && pwd==jsonObj[i]['psw'])
                 window.location.href='/code/'+nhi_id+'.html';
             else{
-               hb3.style.display = 'block';//未在json中找到數據
+               hb3.style.display = 'block';//未在console中找到json數據
             }
         }
     }
